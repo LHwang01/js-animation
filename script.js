0acb5ba1aa0bfc18d05a83c3e1c968302448db5b
LHwang01 <LawrenceHwang01@gmail.com>
2022-03-12 21:58:14 -0500

    Updated First Dialogue Text

--- script.js
+++ script.js
@@ -27,32 +27,32 @@ let frameCount = 0;
 let xPos = -80;
 
 function walkToKnight() {
-    while (true) {
-        frameCount++;
+    // while (true) {
+    //     frameCount++;
 
-        if (frameCount < 15) {
-            window.requestAnimationFrame(walkToKnight);
-            return;
-        }
+    //     if (frameCount < 15) {
+    //         window.requestAnimationFrame(walkToKnight);
+    //         return;
+    //     }
 
-        frameCount = 0;
+    //     frameCount = 0;
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        if (xPos >= 130) {
-            drawFrame(cycleLoop[currentLoopIndex], 17.2, 150, 80);
-            break;
-        }
+    //     if (xPos >= 130) {
+    //         drawFrame(cycleLoop[currentLoopIndex], 17.2, 150, 80);
+    //         break;
+    //     }
 
-        drawFrame(cycleLoop[currentLoopIndex], 20.4, xPos += 20, 80);
-        currentLoopIndex++;
+    //     drawFrame(cycleLoop[currentLoopIndex], 20.4, xPos += 20, 80);
+    //     currentLoopIndex++;
 
-        if (currentLoopIndex >= cycleLoop.length) {
-            currentLoopIndex = 0;
-        }
-    }
+    //     if (currentLoopIndex >= cycleLoop.length) {
+    //         currentLoopIndex = 0;
+    //     }
+    // }
 
-    drawBubble(ctx, 0, 0, 300, 30, 20, "How do I get to the king?", "white", "black", 100, 17);
+    drawBubble(ctx, 0, 0, 300, 30, 20, "Where is King Clovis' Chamber?", "white", "black", 80, 20);
 
     //double while loop
 }
@@ -87,7 +87,7 @@ function drawBubble(ctx, x, y, w, h, radius, text, backgroundColor, textColor, t
 
    ctx.fillStyle = textColor;
 
-   ctx.font = "10px Times New Roman";
+   ctx.font = "12px Times New Roman";
    ctx.fillText(text, textX, textY);
 }
 
