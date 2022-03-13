117ef90c9b2638e41026c95b81d69aa5e15b8968
LHwang01 <LawrenceHwang01@gmail.com>
2022-03-12 21:26:06 -0500

    Made Robed Man Stop and Turn to Knight

--- script.js
+++ script.js
@@ -26,30 +26,37 @@ let currentLoopIndex = 0;
 let frameCount = 0;
 let xPos = -80;
 
-function step() {
-    frameCount++;
-    if (frameCount < 15) {
-        window.requestAnimationFrame(step);
-        return;
+function walkToKnight() {
+    for (let i = 0; i < 3; i++) {
+        frameCount++;
+
+        if (frameCount < 15) {
+            window.requestAnimationFrame(walkToKnight);
+            return;
+        }
+
+        frameCount = 0;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        if (xPos >= 130) {
+            drawFrame(cycleLoop[currentLoopIndex], 17.2, 150, 80);
+            break;
+        }
+
+        drawFrame(cycleLoop[currentLoopIndex], 20.4, xPos += 20, 80);
+        currentLoopIndex++;
+
+        if (currentLoopIndex >= cycleLoop.length) {
+            currentLoopIndex = 0;
+        }
     }
-    frameCount = 0;
+}
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (xPos >= 250) {
-        xPos = -80;
-    }
-
-    drawFrame(cycleLoop[currentLoopIndex], 20.4, xPos += 20, 80);
-    currentLoopIndex++;
-
-    if (currentLoopIndex >= cycleLoop.length) {
-        currentLoopIndex = 0;
-    }
-
-    window.requestAnimationFrame(step);
+function talk() {
 }
 
 function init() {
-    window.requestAnimationFrame(step);
+    walkToKnight();
+    turnAndTalk();
 }
